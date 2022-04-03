92426a4c8bd93bc6828ac05ab59d4b825b23e1c4
Henry <henrypatrichardson@gmail.com>
2022-04-03 17:42:53 -0600

    JS assignmnet

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
- console.log('')
+function buttonClick(){
+    document.getElementById('button').classList.toggle('was-clicked')
+}
+
+function headerClick(e){
+    e.target.classList.toggle('hovered')
+}
+
+function windowLoaded() {
+    console.log('loaded!')
+    document.getElementById('button').addEventListener('click', buttonClick);
+    document.getElementById('h1').addEventListener('mouseover', headerClick);
+}
+
+window.onload = windowLoaded;

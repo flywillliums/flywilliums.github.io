1d32530fbe993654d1dea711d39a2a1aff17c25d
Henry <henrypatrichardson@gmail.com>
2022-04-10 20:38:36 -0600

    sike finished it this time

--- js/assignment12.js
+++ js/assignment12.js
@@ -3,6 +3,12 @@ function getQuote() {
     $.getJSON("https://api.whatdoestrumpthink.com/api/v1/quotes/random/", function(response){
       var DonQuote = JSON.stringify(response.message);
       $("#js-quote-text").html("<b><i>"+DonQuote+"</i></b>");
+      var authors = ["mini hands","donald twump","a racist",
+                   "poop meister","orange julius","sir dookie donald",
+                   "lord voldemort","butt sniffer","swampy johns","plastic man", 
+                   "butt scratcher"]
+    var rand = Math.floor(Math.random()* (authors.length));
+    $("#authorHere").html("<b><i>"+" -"+authors[rand]+"</i></b>");
     });
   }
   $(document).ready(function() {
